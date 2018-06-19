702883753c0fff4fdfda56935547afde05cde456
gsw91 <g.wojcik@vp.pl>
2018-06-19 10:51:44 +0200

    Library REST app

--- src/main/java/com/crud/library/domain/Title.java
+++ src/main/java/com/crud/library/domain/Title.java
@@ -1,15 +1,10 @@
 package com.crud.library.domain;
 
-import com.fasterxml.jackson.datatype.jsr310.deser.YearDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.YearSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.type.descriptor.sql.IntegerTypeDescriptor;
 
 import javax.persistence.*;
-import java.sql.Date;
-import java.time.Year;
 
 @AllArgsConstructor
 @NoArgsConstructor
